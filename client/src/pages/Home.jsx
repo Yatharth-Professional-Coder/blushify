4d8ddf78c41564673e5afc726d4365e596531089
Yatharth <yatharth@Yatharths-MacBook-Air.local>
2026-01-29 19:59:34 +0530

    image and dark theme

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const categories = [
     { name: 'Skin Care', image: 'https://images.unsplash.com/photo-1616394584738-fc6e612e71b9?w=300&h=300&fit=crop' },
     { name: 'Bridal', image: 'https://swissbeauty.in/cdn/shop/files/bridal_section_Thumbnail_1_1080x.jpg?v=1736341692' },
-    { name: 'Makeup', image: 'https://www.britannica.com/story/why-did-we-start-wearing-makeup' },
+    { name: 'Makeup', image: 'https://cdn.britannica.com/35/222035-050-C68AD682/makeup-cosmetics.jpg' },
     { name: 'Lips', image: 'https://swissbeauty.in/cdn/shop/files/3_1-LIPS_140x140.png?v=1747133814' },
     { name: 'Eyes', image: 'https://swissbeauty.in/cdn/shop/files/2a_b0e928b4-c1b1-4e4f-9a11-4c32c8c984f6_140x140.png?v=1663159924' },
 ];
